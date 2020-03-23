b4b4a03257b34e97efe14f6fb25ec7497de046e9
dshriner <42812753+dshriner@users.noreply.github.com>
2020-03-23 08:14:26 -0400

    Update Procrustes.R

--- Procrustes.R
+++ Procrustes.R
@@ -1,5 +1,11 @@
+#Daniel Shriner
+#3/23/20
+#implementation of Procrustes analysis using principal coordinate analysis
+#comparison of Fst and geographic distance matrices of equal dimensions
+
 dat1 <- read.table("fst.txt",header=FALSE)
 dat2 <- read.table("geographic_distances.txt",header=FALSE)
+reps <- 1e9
 Xloc <- cmdscale(as.dist(dat1), k = nrow(dat1)-1,eig=TRUE,add=TRUE)
 Yloc <- cmdscale(as.dist(dat2), k = nrow(dat2)-1,eig=TRUE,add=TRUE)
 Xcentroid <- Xloc$points[,c(1:2)]
@@ -8,8 +14,8 @@ C <- t(Ycentroid)%*%Xcentroid
 Csvd <- svd(C)
 D <- Csvd$d
 statistic <- sqrt((sum(D)^2)/(sum(diag(t(Xcentroid)%*%Xcentroid))*sum(diag(t(Ycentroid)%*%Ycentroid))))
-nulldist <- vector("numeric",1e9)
-for (i in 1:1e9) {
+nulldist <- vector("numeric",reps)
+for (i in 1:reps) {
 	o <- sample(nrow(dat1))
 	Xperm <- Xcentroid[o,]
 	C <- t(Ycentroid)%*%Xperm
